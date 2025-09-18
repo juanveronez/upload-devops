e98941ec04d969a3c2f40a4d2e4c1bafaae5173a
Juan Veronez <juan.monteirov@gmail.com>
2025-09-18 17:13:10 -0300

    feat: add a ecr creation into the IaC

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 import * as aws from "@pulumi/aws";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("primeiro-teste-pos-rocketseat", {
-  bucket: 'primeiro-teste-pos-rocketseat-bkt', // using this the name will not be primeiro-teste-pos-rocketseat-<hash>
+const bucket = new aws.s3.Bucket("bucket-teste-pos-rocketseat", {
+  bucket: 'bucket-teste-pos-rocketseat', // using this the name will not be primeiro-teste-pos-rocketseat-<hash>
   tags: {
     IAC: "true", // boa pratica para mostrar que o recurso eh gerido por iac, entao se alguem tentar mudar no console pode saber que o recurso sera sobrescrito em algum momento pelo iac
   }
+})
+
+const ecr = new aws.ecr.Repository('ecr-teste-pos-rocketseat', {
+   name: 'ecr-teste-pos-rocketseat',
+   imageTagMutability: 'IMMUTABLE',
+   tags: {
+    IAC: "true",
+   }
 })
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
 export const bucketRegion = bucket.region;
 export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name
+export const ecrRepositoryUrl = ecr.repositoryUrl
