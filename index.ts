da7431e36a1b63c60f6ca9df5f5581ad1cb49295
Juan Veronez <juan.monteirov@gmail.com>
2025-09-18 18:27:57 -0300

    feat: update iac to create two s3 buckets

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import * as aws from "@pulumi/aws";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("bucket-teste-pos-rocketseat", {
+const firstBucket = new aws.s3.Bucket("bucket-teste-pos-rocketseat", {
   bucket: 'bucket-teste-pos-rocketseat', // using this the name will not be primeiro-teste-pos-rocketseat-<hash>
   tags: {
     IAC: "true", // boa pratica para mostrar que o recurso eh gerido por iac, entao se alguem tentar mudar no console pode saber que o recurso sera sobrescrito em algum momento pelo iac
+  }
+})
+
+const secondBucket = new aws.s3.Bucket("second-bucket-teste-pos-rocketseat", {
+  bucket: 'second-bucket-teste-pos-rocketseat',
+  tags: {
+    IAC: "true",
   }
 })
 
@@ -17,9 +24,13 @@ const ecr = new aws.ecr.Repository('ecr-teste-pos-rocketseat', {
 })
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
-export const bucketRegion = bucket.region;
-export const bucketArn = bucket.arn;
+export const firstBucketName = firstBucket.id;
+export const firstBucketRegion = firstBucket.region;
+export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name
 export const ecrRepositoryUrl = ecr.repositoryUrl
